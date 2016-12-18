8edb8aae0dbe5f8500291a3ba278f03b20939fb4
arashparnia <arashparnia@gmail.com>
2016-12-18 21:48:22 +0100

    frame template stage 1

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 /**
  * Created by arashparnia on 18/12/2016.
  */
 public class MainFrame {
+    public static void main(String[] args) {
+        JFrame main_frame = new JFrame("MainFrame");
+//        main_frame.setContentPane(new MainFrame());
+        main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main_frame.pack();
+        main_frame.setVisible(true);
+
+
+    }
 }
+
+
