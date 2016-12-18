1e20cb0de0e0b05d4adb4e9986006c231390ce63
arashparnia <arashparnia@gmail.com>
2016-12-18 22:46:29 +0100

    frame template stage 1

--- src/Starter.java
+++ src/Starter.java
@@ -1,8 +1,21 @@
+import javax.swing.*;
+
 /**
  * Created by arashparnia on 18/12/2016.
  */
 public class Starter {
     public static void main(String[] args) {
 
+        //Create and set up the window.
+        JFrame main_frame = new MainFrame();
+        main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        main_frame.setJMenuBar(main_frame.getJMenuBar());
+        main_frame.setContentPane(main_frame.getContentPane());
+
+        //Display the window.
+        main_frame.setSize(450, 260);
+        main_frame.setVisible(true);
     }
 }
